9efd4bcc024be056f6374824a653d589572c7fb0
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-08-04 21:28:15 -0300

    Restição de rotas por login e page 404

---
+++ frontend/src/pages/page404/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function page404() {
+  return (
+    <>
+      <h1>Page not found!</h1>
+      <h2>error: 404</h2>
+    </>
+  );
+}
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -9,16 +10,20 @@ import Details from '../pages/Details';
 
 import Profile from '../pages/Profile';
 
+import Page404 from '../pages/page404';
+
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/details" component={Details} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/details" component={Details} isPrivate />
 
-      <Route path="/profile" component={Profile} />
+      <Route path="/profile" component={Profile} isPrivate />
+
+      <Route path="/" component={() => <Page404 />} />
     </Switch>
   );
 }
