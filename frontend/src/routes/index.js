ea6fe81fa513372a3cecf73f74d2f76e40567a18
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-08-04 21:00:00 -0300

    Criação e configuração de rotas

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Details from '../pages/Details';
+
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/details" component={Details} />
+
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
