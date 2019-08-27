a28500dc743d44d7c2f624445f498be67a1cb2bf
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-08-27 11:35:01 -0300

    Migrations, models e routes de Arquivos, Meetups e Inscrições / Finalizar

---
+++ server/src/app/controllers/MeetupController.js
@@ -0,0 +1,142 @@
+import * as Yup from 'yup';
+
+import {
+  isBefore,
+  startOfHour,
+  parseISO,
+  startOfDay,
+  endOfDay,
+} from 'date-fns';
+import { Op } from 'sequelize';
+
+import Meetup from '../models/Meetup';
+import File from '../models/File';
+import User from '../models/User';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string()
+        .required()
+        .min(5),
+      description: Yup.string()
+        .required()
+        .min(15),
+      location: Yup.string()
+        .required()
+        .min(10),
+      date_hour: Yup.date().required(),
+      file_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const file = await File.findByPk(req.body.file_id);
+    if (!file) {
+      return res.json({ error: 'File not found' });
+    }
+
+    const hourStart = startOfHour(parseISO(req.body.date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ error: `It's impossible to create meetups on past dates` });
+    }
+
+    const user_id = req.userId;
+    const all = await Meetup.create({
+      ...req.body,
+      user_id,
+    });
+
+    return res.json(all);
+  }
+
+  async update(req, res) {
+    const { userId } = req;
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (!meetup) {
+      return res.status(400).json({ error: 'Meetup not found' });
+    }
+
+    if (meetup.user_id !== userId) {
+      return res
+        .status(400)
+        .json({ error: 'User id is not equals to creator id' });
+    }
+
+    const hourStart = startOfHour(parseISO(meetup.date_hour));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ error: `It's impossible to change finished meetups` });
+    }
+
+    await meetup.update(req.body);
+
+    return res.json(meetup);
+  }
+
+  async index(req, res) {
+    const {
+      // formato YYYY-MM-DD
+      date = new Date().toISOString().split('T')[0],
+      page = 1,
+    } = req.query;
+
+    const parsedDate = parseISO(date);
+
+    const meetups = await Meetup.findAll({
+      where: {
+        date_hour: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      limit: 10,
+      offset: (page - 1) * 10,
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
+
+    return res.json(meetups);
+  }
+
+  async delete(req, res) {
+    const { userId } = req;
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (!meetup) {
+      return res.status(400).json({ error: 'Meetup not found' });
+    }
+
+    if (meetup.user_id !== userId) {
+      return res
+        .status(400)
+        .json({ error: `It's impossible to delete meetups from other users` });
+    }
+
+    const hourStart = startOfHour(parseISO(meetup.date_hour));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ error: `It's impossible to delete finished meetups` });
+    }
+
+    meetup.destroy();
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,14 +14,15 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.get('/meetups', MeetupController.index);
 
 // Miiddleware de autenticação
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/meetups', MeetupController.store);
 
 export default routes;
